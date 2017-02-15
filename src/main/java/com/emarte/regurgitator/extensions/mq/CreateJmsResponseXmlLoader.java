3f8ee2c3c9ca626752c465e493da41c91d786f75
talmeym <mtalmey@gmail.com>
2017-02-15 12:14:12 +0000

    Changed to use jvm supplied xml / xpath processing

--- src/main/java/com/emarte/regurgitator/extensions/mq/CreateJmsResponseXmlLoader.java
+++ src/main/java/com/emarte/regurgitator/extensions/mq/CreateJmsResponseXmlLoader.java
@@ -1,21 +1,22 @@
 package com.emarte.regurgitator.extensions.mq;
 
 import com.emarte.regurgitator.core.*;
-import org.dom4j.Element;
+import org.w3c.dom.Element;
 
 import java.util.Set;
 
+import static com.emarte.regurgitator.core.XmlConfigUtil.getAttribute;
 import static com.emarte.regurgitator.extensions.mq.ExtensionsMqConfigConstants.*;
 
 public class CreateJmsResponseXmlLoader implements XmlLoader<CreateJmsResponse> {
-	CreateResponseXmlLoader createResponseXmlLoader = new CreateResponseXmlLoader();
+	private CreateResponseXmlLoader createResponseXmlLoader = new CreateResponseXmlLoader();
 
 	@Override
 	public CreateJmsResponse load(Element element, Set<Object> allIds) throws RegurgitatorException {
 		CreateResponse response = (CreateResponse) createResponseXmlLoader.load(element, allIds);
-		String destination = element.attributeValue(JMS_DESTINATION);
-		String correlationId = element.attributeValue(JMS_CORRELATION_ID);
-		String type = element.attributeValue(JMS_TYPE);
+		String destination = getAttribute(element, JMS_DESTINATION);
+		String correlationId = getAttribute(element, JMS_CORRELATION_ID);
+		String type = getAttribute(element, JMS_TYPE);
 		return new CreateJmsResponse(response, destination, correlationId, type);
 	}
 }
