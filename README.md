9abf479e63efabfa91b7262849f2f285fffbe6a4
talmeym <mtalmey@gmail.com>
2017-08-12 19:10:54 +0100

    worked on mq messaging, added log statements, added README content, updated create jms response to allow quickset of more params

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 regurgitator is a modular, light-weight, extendable java-based processing framework designed to 'regurgitate' canned or clever responses to incoming requests; useful for mocking or prototyping services.
 
 start your reading here: [regurgitator-all](http://github.com/talmeym/regurgitator-all#regurgitator)
+
+## extension mq steps in xml
+
+### create-jms-response
+
+a create-jms-response step returns a response from regurgitator, allowing ``response-metadata`` parameters to be set for ``jms-destination``, ``correlation-id`` and ``jms-type``:
+
+```xml
+<rgw:create-http-response source="response" jms-message-id="message-id" jms-type="my-jms-type" jms-destination="queue://my-destination" correlation-id="52908d79-b7c6-4e18-ab13-e991d1b67d8b" jms-delivery-mode=4 jms-expiration="" jms-redelivered=true jms-reply-to="queue://other-destination" jms-timestamp=123456789 />
+```
--- src/main/java/com/emarte/regurgitator/extensions/mq/CreateJmsResponseXmlLoader.java
+++ src/main/java/com/emarte/regurgitator/extensions/mq/CreateJmsResponseXmlLoader.java
@@ -14,9 +14,16 @@ public class CreateJmsResponseXmlLoader implements XmlLoader<CreateJmsResponse> 
 	@Override
 	public CreateJmsResponse load(Element element, Set<Object> allIds) throws RegurgitatorException {
 		CreateResponse response = (CreateResponse) createResponseXmlLoader.load(element, allIds);
+		String messageId = getAttribute(element, JMS_MESSAGE_ID);
+		String type = getAttribute(element, JMS_TYPE);
 		String destination = getAttribute(element, JMS_DESTINATION);
 		String correlationId = getAttribute(element, JMS_CORRELATION_ID);
-		String type = getAttribute(element, JMS_TYPE);
-		return new CreateJmsResponse(response, destination, correlationId, type);
+		String deliveryMode = getAttribute(element, JMS_DELIVERY_MODE);
+		String expiration = getAttribute(element, JMS_EXPIRATION);
+		String priority = getAttribute(element, JMS_PRIORITY);
+		String redelivered = getAttribute(element, JMS_REDELiVERED);
+		String replyTo = getAttribute(element, JMS_REPLY_TO);
+		String timestamp = getAttribute(element, JMS_TIMESTAMP);
+		return new CreateJmsResponse(response, messageId, type, destination, correlationId, deliveryMode, expiration, priority, redelivered, replyTo, timestamp);
 	}
 }
