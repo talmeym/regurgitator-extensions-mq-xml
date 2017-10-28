18cc16216fb15e8ec155bb752f6ef2ea5ff4b0df
talmeym <mtalmey@gmail.com>
2017-10-28 18:35:56 +0100

    standardised method names in config utils, fixed types in create jms response

--- src/main/java/com/emarte/regurgitator/extensions/mq/CreateJmsResponseXmlLoader.java
+++ src/main/java/com/emarte/regurgitator/extensions/mq/CreateJmsResponseXmlLoader.java
@@ -9,7 +9,7 @@ import org.w3c.dom.Element;
 
 import java.util.Set;
 
-import static com.emarte.regurgitator.core.XmlConfigUtil.getAttribute;
+import static com.emarte.regurgitator.core.XmlConfigUtil.*;
 import static com.emarte.regurgitator.extensions.mq.ExtensionsMqConfigConstants.*;
 
 public class CreateJmsResponseXmlLoader implements XmlLoader<CreateJmsResponse> {
@@ -18,16 +18,16 @@ public class CreateJmsResponseXmlLoader implements XmlLoader<CreateJmsResponse> 
     @Override
     public CreateJmsResponse load(Element element, Set<Object> allIds) throws RegurgitatorException {
         CreateResponse response = (CreateResponse) createResponseXmlLoader.load(element, allIds);
-        String messageId = getAttribute(element, JMS_MESSAGE_ID);
-        String type = getAttribute(element, JMS_TYPE);
-        String destination = getAttribute(element, JMS_DESTINATION);
-        String correlationId = getAttribute(element, JMS_CORRELATION_ID);
-        String deliveryMode = getAttribute(element, JMS_DELIVERY_MODE);
-        String expiration = getAttribute(element, JMS_EXPIRATION);
-        String priority = getAttribute(element, JMS_PRIORITY);
-        String redelivered = getAttribute(element, JMS_REDELIVERED);
-        String replyTo = getAttribute(element, JMS_REPLY_TO);
-        String timestamp = getAttribute(element, JMS_TIMESTAMP);
+        String messageId = loadOptionalStr(element, JMS_MESSAGE_ID);
+        String type = loadOptionalStr(element, JMS_TYPE);
+        String destination = loadOptionalStr(element, JMS_DESTINATION);
+        String correlationId = loadOptionalStr(element, JMS_CORRELATION_ID);
+        Integer deliveryMode = loadOptionalInt(element, JMS_DELIVERY_MODE);
+        Long expiration = loadOptionalLong(element, JMS_EXPIRATION);
+        Integer priority = loadOptionalInt(element, JMS_PRIORITY);
+        Boolean redelivered = loadOptionalBool(element, JMS_REDELIVERED);
+        String replyTo = loadOptionalStr(element, JMS_REPLY_TO);
+        Long timestamp = loadOptionalLong(element, JMS_TIMESTAMP);
         return new CreateJmsResponse(response, messageId, type, destination, correlationId, deliveryMode, expiration, priority, redelivered, replyTo, timestamp);
     }
 }
